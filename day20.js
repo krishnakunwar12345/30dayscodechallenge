901b3f41731c73e6899e4e03547e26dd8a051abf
krishnakunwar12345 <109654201+krishnakunwar12345@users.noreply.github.com>
2023-09-24 21:19:28 +0530

    day20

---
+++ day20.js
@@ -0,0 +1,24 @@
+// // Find all possible sub array for the given array  [1,2,3]
+/*To find all possible subarrays for the given array [1, 2, 3], you can generate all combinations of 
+elements. Here are all the possible subarrays*/
+[1]
+[2]
+[3]
+[1, 2]
+[2, 3]
+[1, 2, 3]
+
+// These are all the non-empty subarrays of the array [1, 2, 3]. Remember that a subarray is a contiguous sequence of elements from the original array
+
+// non-empty subarray- it is a contiguous sequence of elements from an array that contains at least one element.
+
+
+
+
+
+
+
+
+
+
+
