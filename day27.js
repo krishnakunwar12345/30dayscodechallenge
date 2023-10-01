aa21af58a5d58c76e8d24f8488306ed88ee70add
krishnakunwar12345 <109654201+krishnakunwar12345@users.noreply.github.com>
2023-10-01 21:09:29 +0530

    day27

---
+++ day27.js
@@ -0,0 +1,38 @@
+//Difference between high level and low level language
+/*In the context of JavaScript, it's not common to refer to it as a high-level or low-level language 
+because JavaScript itself is a high-level programming language. However, let me explain the general 
+concepts of high-level and low-level languages, and then I'll clarify JavaScript's position.
+
+High-Level Language:
+
+Abstraction: High-level languages are designed with a strong focus on abstraction. They provide abstractions that are closer to human language and concepts, making it easier for programmers to write code with less concern for low-level details.
+
+Readability: High-level languages are typically more readable and understandable by humans. They use meaningful keywords and structures, which makes the code easier to write, understand, and maintain.
+
+Portability: Code written in high-level languages is often more portable, meaning it can be executed on different platforms without major modifications, thanks to compilers or interpreters that handle platform-specific details.
+
+Productivity: High-level languages often result in faster development and higher programmer productivity because they abstract away many low-level details and provide libraries and frameworks for common tasks.
+
+
+Low-Level Language:
+
+Abstraction: Low-level languages, such as Assembly language, are closer to the hardware and provide little to no abstraction. Programmers need to work with low-level details like memory addresses, registers, 
+and specific CPU instructions.
+
+Readability: Low-level languages are less readable and harder to understand for humans compared to high-level languages because they involve cryptic symbols and instructions.
+
+Performance: Low-level languages can offer more control over hardware resources and can be highly optimized for performance. They are often used for system-level programming and tasks where efficiency is critical.
+
+Portability: Code written in low-level languages is less portable because it is tied closely to the hardware architecture of a specific platform. Porting code from one platform to another can be a complex and time-consuming task
+
+Now, back to JavaScript:JavaScript is a high-level programming language. It is designed to be easy to read, write, and understand. It abstracts many low-level details, such as memory management and hardware-specific operations, away from the programmer. 
+JavaScript code is typically executed by a JavaScript engine, which handles platform-specific details, making it relatively portable across different browsers and platforms.
+So, in the context of JavaScript, it falls squarely into the high-level language category, as it provides a high level of abstraction and focuses on programmer productivity and ease of use.*/
+
+//Is Js is high level or low level language 
+/*JavaScript (JS) is a high-level programming language. It is designed to be user-friendly, with a focus on providing 
+abstractions that make it easier for developers to write and maintain code. In JavaScript, you work with variables, objects, 
+functions, and other high-level constructs, which abstract away low-level details like memory management and hardware-specific operations.
+Low-level languages, on the other hand, are closer to the hardware and require programmers to work with low-level details 
+such as memory addresses, registers, and specific CPU instructions. JavaScript is a high-level language because it abstracts 
+these low-level complexities and provides a more human-readable and understandable syntax.In summary, JavaScript is definitively classified as a high-level programming language.*/
